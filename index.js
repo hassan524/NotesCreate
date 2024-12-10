a997615e708d4ce276119e437782170ad6dbac6c
Hassan Rehan <hrehan752@gmail.com>
2024-12-10 22:59:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
